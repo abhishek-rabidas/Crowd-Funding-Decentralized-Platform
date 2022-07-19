fc1415108948fc6110141c4fc6cdae40c7543a98
abhishek <avisec07@gmail.com>
2022-07-19 18:11:18 +0530

    deploy testing with contract method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import Dashboard from './frontend/views/Dashboard';
+import { ethers } from 'ethers';
+import abi from "../src/backend/output/Donation.json";
+import {useState} from "react";
 
 function App() {
-  return (
-    <Dashboard />
-  );
+  const [contract, setContract] = useState({});
+  const [account, setAccount] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [connectButtonVisible, setConnectButtonVisible] = useState("block");
+
+  const web3Handler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0])
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    const signer = provider.getSigner()
+
+    window.ethereum.on('chainChanged', (chainId) => {
+      window.location.reload();
+    })
+
+    window.ethereum.on('accountsChanged', async function (accounts) {
+      setAccount(accounts[0])
+      await web3Handler()
+    })
+    loadContracts(signer)
+  }
+
+  function loadContracts(signer) {
+    const contract = new ethers.Contract(abi.networks[5777].address, abi.abi, signer);
+    setContract(contract);
+    setLoading(false);
+    setConnectButtonVisible("none")
+  }
+
+  return (<>
+  <button style={{backgroundColor: "black", color: "white", margin: "3rem", padding: "1rem", display: connectButtonVisible}} onClick={web3Handler}>Connect Wallet</button>
+  {
+    loading ? <p>Awaiting meta mask connection </p> : <Dashboard contract={contract} />
+  }
+  </>);
 }
 
 export default App;
--- src/frontend/views/Dashboard.jsx
+++ src/frontend/views/Dashboard.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-export default function Dashboard() {
+export default function Dashboard({ contract }) {
+    const [donorName, setDonorName] = React.useState("");
+    const [beneficiaryName, setBeneficiaryName] = React.useState("");
+
+    async function registerDonor() {
+        console.log("Registering donor: " + donorName);
+        await (contract.register_donor(donorName));
+    }
+
+    async function registerBeneficiary() {
+        console.log("Registering Beneficiary: " + beneficiaryName);
+        await (contract.register_beneficiary(beneficiaryName));
+    }
+
     return (<>
-        <h1>Crowd Funding DApp</h1>
+        <input type="text" onChange={(e) => {
+            setDonorName(e.target.value);
+        }} placeholder="Enter the donor name" />
+        <button onClick={registerDonor}>Add Donor</button>
+
+        <br />
+
+        <input type="text" onChange={(e) => {
+            setBeneficiaryName(e.target.value);
+        }} placeholder="Enter the beneficiary name" />
+        <button onClick={registerBeneficiary}>Add Beneficiary</button>
     </>);
 }
